8a92a200cba57c62d62bf67d7fee144285380337
sri varshini <srivarshsundarajan2003@gmail.com>
2026-03-16 11:53:38 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,11 +1,7 @@
-# urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Path for submitting exam answers
     path('submit/', views.submit, name='submit'),
-    
-    # Path for showing exam results for a specific user
-    path('result/<str:user_name>/', views.show_exam_result, name='show_exam_result'),
+    path('show_exam_result/', views.show_exam_result, name='show_exam_result'),
 ]
